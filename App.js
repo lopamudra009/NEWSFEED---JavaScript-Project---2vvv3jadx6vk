eca5a0b4c56f87f239951b3c5597cbb6a72b6f60
lopamudra009 <lopamudrapanda009@gmail.com>
2023-05-14 01:27:03 +0530

    Updated code

--- App.js
+++ App.js
@@ -1,9 +1,66 @@
 
-fetch('https://inshorts.deta.dev/news?category=science')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
+// fetch('https://inshorts.deta.dev/news?category=science')
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data)
+//     })
 
+function Business() {
+    fetch('https://inshorts.deta.dev/news?category=business')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+}
 
+function All() {
+    fetch('https://inshorts.deta.dev/news?category=all')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+}
+function Sports() {
+    fetch('https://inshorts.deta.dev/news?category=sports')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+}
+function Worlds() {
+    fetch('https://inshorts.deta.dev/news?category=world')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+}
+function Politics() {
+    fetch('https://inshorts.deta.dev/news?category=politics')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+}
 
+function Hatke() {
+    fetch('https://inshorts.deta.dev/news?category=hatke')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+}
+
+function Science() {
+    fetch('https://inshorts.deta.dev/news?category=science')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+}
+function Automobile() {
+    fetch('https://inshorts.deta.dev/news?category=automobile')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+}
